56a1c4972f7e69108f2cbc536cfe5db824697a75
gsn97 <186332589+gsn97@users.noreply.github.com>
2025-10-14 18:43:40 -0400

    Wrote the pseudocode for the project

---
+++ script.js
@@ -0,0 +1,39 @@
+// Pseudocode Algorithm:
+// 1. Create string Variable for user Input (userChoice)
+// 2. Create string Variable for Computer Choice (cpuChoice)
+// 3. Create integer Variable for User Score (userScore), default value should be 0
+// 4. Create integer Variable for Computer Score (cpuScore), default value should be 0
+// 5. Create Function that prompts the user for a choice (Rock, Paper, or Scissors) (getUserChoice) (make sure it's case insensitive)
+//     - Plug that result into variable userChoice
+//     - Print userChoice to the console with the message "You have chosen x"
+// 6. Create Function for Computer Choice by using a randomizer to generate Rock, Paper, or Scissors (getCpuChoice)
+//     - Plug that into variable cpuChoice
+//     - Print cpuChoice to the console with the message "Computer chooses y"
+// 7. Create a Function called playRound (with two parameters: userChoice and cpuChoice) (it should encompass the following) 
+//         - If userChoice === cpuChoice, then print to console "It's a tie!"
+//             - Print a score message
+//         - If userChoice is Rock and cpuChoice is Scissors:
+//             - Add 1 to userScore and return it
+//             - Print to console "You win! Rock crushes Scissors!"
+//             - Print a score message
+//         - If userChoice is Rock and cpuChoice is Paper:
+//             - Add 1 to cpuScore and return it
+//             - Print to console "You lose! Paper covers Rock!"
+//             - Print a score message
+//         - If userChoice is Paper and cpuChoice is Rock:
+//             - Add 1 to userScore and return it
+//             - Print to console "You win! Paper Covers Rock!"
+//             - Print a score message
+//         - If userChoice is Paper and cpuChoice is Scissors:
+//             - Add 1 to cpuScore and return it
+//             - Print to console "You lose! Scissors cuts Paper!"
+//             - Print a score message
+//         - If userChoice is Scissors and cpuChoice is Paper:
+//             - Add 1 to userScore and return it
+//             - Print to console "You win! Scissors cuts Paper!"
+//             - Print a score message
+//         - If userChoice is Scissors and cpuChoice is Rock:
+//             - Add 1 to cpuScore and return it
+//             - Print to console "You lose! Rock crushes Scissors!"
+//             - Print a score message
+// 8. Create a function called playGame that calls playRound 5 times, and then displays a final score with the winner
